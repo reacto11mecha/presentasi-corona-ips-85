86750783e97084a55af77dc84185f230a27e71f3
EzraKhairanPermana <ezrakhairan123@gmail.com>
2020-04-07 19:55:33 +0700

    Update script.js | menambahkan event fetch ke data corona

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,60 @@
-window.ws = new WebSlides();
+const slide = new WebSlides();
+
+const typeChart = {
+  bar: {
+    loader: document.querySelector(".loader#bar"),
+    canvas: document.querySelector("canvas#barProvinsi"),
+  },
+};
+
+let Chart1 = null;
+let Chart2 = null;
+
+fetch("https://api.kawalcorona.com/indonesia/provinsi/")
+  .then((res) => res.json())
+  .then((result) => {
+    let sembuh = [];
+    let positif = [];
+    let meninggal = [];
+
+    let labels = [];
+
+    const { loader, canvas } = typeChart.bar;
+
+    result.forEach((res) => {
+      const data = res.attributes;
+
+      positif.push(data.Kasus_Posi);
+      sembuh.push(data.Kasus_Semb);
+      meninggal.push(data.Kasus_Meni);
+
+      labels.push(data.Provinsi);
+    });
+
+    loader.remove();
+
+    myChartBar = new Chart(canvas, {
+      type: "bar",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Positif",
+            data: positif,
+            backgroundColor: "#ffc107",
+          },
+          {
+            label: "Sembuh",
+            data: sembuh,
+            backgroundColor: "#28a745",
+          },
+          {
+            label: "Meninggal",
+            data: meninggal,
+            backgroundColor: "#dc3545",
+          },
+        ],
+      },
+    });
+  })
+  .catch((err) => {});
