8722dbc1f280abf0e63330758ac09aa76256bd96
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-05-09 22:33:32 +0700

    Updates

--- 327.bundle.js
+++ 327.bundle.js
@@ -1,18 +1,1 @@
-(self["webpackChunkppt_ips_coronavirus_85"] = self["webpackChunkppt_ips_coronavirus_85"] || []).push([[327],{
-
-/***/ 327:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "fetcher": () => /* binding */ fetcher
-/* harmony export */ });
-const fetcher = (url) => fetch(url).then((response) => response.json());
-
-
-
-
-/***/ })
-
-}]);
+(self.webpackChunkppt_ips_coronavirus_85=self.webpackChunkppt_ips_coronavirus_85||[]).push([[327],{327:(s,e,p)=>{"use strict";p.r(e),p.d(e,{fetcher:()=>c});const c=s=>fetch(s).then((s=>s.json()))}}]);
---
+++ 743.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunkppt_ips_coronavirus_85=self.webpackChunkppt_ips_coronavirus_85||[]).push([[743],{488:(p,s,e)=>{"use strict";p.exports=e.p+"corona.jpeg"},937:(p,s,e)=>{"use strict";p.exports=e.p+"pesebaran-corona.jpg"}}]);
